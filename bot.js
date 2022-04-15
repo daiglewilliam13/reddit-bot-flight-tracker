4991175df239e3067e95d469d0cef7d534bef12a
William Daigle <daiglewilliam13@gmail.com>
2022-04-15 13:55:53 -0700

    update twitter api

--- bot.js
+++ bot.js
@@ -1,5 +1,12 @@
 require('dotenv').config();
+const  { TwitterApi } = require('twitter-api-v2');
 
+const client = new TwitterApi({
+    appKey: process.env.TWITTER_API_KEY,
+    appSecret: process.env.TWITTER_API_SECRET,
+    accessToken: process.env.TWITTER_ACCESS_TOKEN,
+    accessSecret: process.env.TWITTER_TOKEN_SECRET,
+})
 const Snoowrap = require('snoowrap');
 const { CommentStream } = require('snoostorm');
 
@@ -11,11 +18,77 @@ const r = new Snoowrap({
     password: process.env.REDDIT_PASS
 });
 
+const needle = require('needle');
+const userId = "1268280560599867392";
+const url = `https://api.twitter.com/2/users/${userId}/tweets`;
 
+// The code below sets the bearer token from your environment variables
+// To set environment variables on macOS or Linux, run the export command below from the terminal:
+// export BEARER_TOKEN='YOUR-TOKEN'
+const bearerToken = process.env.BEARER_TOKEN;
 
-r.getSubreddit('whereiselon')
-.submitSelfpost({title:'test post please ignore', text: "hello world"})
-.sticky()
-.approve()
-.assignFlair({text: 'test flair hello'})
-.reply('test comment');
+const getUserTweets = async () => {
+    let userTweets = [];
+
+    // we request the author_id expansion so that we can print out the user name later
+    let params = {
+        "max_results": 100,
+        "tweet.fields": "created_at",
+        "expansions": "author_id"
+    }
+
+    const options = {
+        headers: {
+            "User-Agent": "v2UserTweetsJS",
+            "authorization": `Bearer ${bearerToken}`
+        }
+    }
+
+    let hasNextPage = true;
+    let nextToken = null;
+    let userName;
+    console.log("Retrieving Tweets...");
+
+    while (hasNextPage) {
+        let resp = await getPage(params, options, nextToken);
+        if (resp && resp.meta && resp.meta.result_count && resp.meta.result_count > 0) {
+            userName = resp.includes.users[0].username;
+            if (resp.data) {
+                userTweets.push.apply(userTweets, resp.data);
+            }
+            if (resp.meta.next_token) {
+                nextToken = resp.meta.next_token;
+            } else {
+                hasNextPage = false;
+            }
+        } else {
+            hasNextPage = false;
+        }
+    }
+
+    console.dir(userTweets, {
+        depth: null
+    });
+    console.log(`Got ${userTweets.length} Tweets from ${userName} (user ID ${userId})!`);
+
+}
+
+const getPage = async (params, options, nextToken) => {
+    if (nextToken) {
+        params.pagination_token = nextToken;
+    }
+
+    try {
+        const resp = await needle('get', url, params, options);
+
+        if (resp.statusCode != 200) {
+            console.log(`${resp.statusCode} ${resp.statusMessage}:\n${resp.body}`);
+            return;
+        }
+        return resp.body;
+    } catch (err) {
+        throw new Error(`Request failed: ${err}`);
+    }
+}
+
+getUserTweets();
