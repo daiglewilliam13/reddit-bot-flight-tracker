1601201c264f913bb2578225c3ba8c95071c0e80
William Daigle <daiglewilliam13@gmail.com>
2022-04-15 10:46:05 -0700

    fix login credentials

--- bot.js
+++ bot.js
@@ -4,19 +4,18 @@ const Snoowrap = require('snoowrap');
 const { CommentStream } = require('snoostorm');
 
 const r = new Snoowrap({
-    userAgent: 'muskflighttrackerbot',
+    userAgent: process.env.REDDIT_USERAGENT,
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
     username: process.env.REDDIT_USER,
     password: process.env.REDDIT_PASS
 });
 
-const stream = new CommentStream(r, {
-    subreddit: "testingground4bots",
-    limit: 10,
-    pollTime: 10000
-});
 
-stream.on("item", comment => {
-    console.log(comment)
-})
+
+r.getSubreddit('whereiselon')
+.submitSelfpost({title:'test post please ignore', text: "hello world"})
+.sticky()
+.approve()
+.assignFlair({text: 'test flair hello'})
+.reply('test comment');
