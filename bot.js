7bad7ab739ff8bbb11dc74b971618b9d5c944151
William Daigle <daiglewilliam13@gmail.com>
2022-05-02 09:09:10 -0700

    update most recent tweet

--- bot.js
+++ bot.js
@@ -10,7 +10,7 @@ const r = new Snoowrap({
 });
 
 const needle = require('needle');
-const userId = "1268280560599867392";
+const userId = "1520220213614153728";
 const url = `https://api.twitter.com/2/users/${userId}/tweets`;
 
 const bearerToken = process.env.BEARER_TOKEN;
